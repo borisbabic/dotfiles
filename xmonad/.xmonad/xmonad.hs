6237153bdd0bf6e422e2f4b35e0ab9daab9b82db
Boris Babic <boris.ivan.babic@gmail.com>
2017-02-25 22:17:46 +0100

    XMONAD: Add xmonad

---
+++ xmonad/.xmonad/xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Config.Kde
+import XMonad.Hooks.ManageDocks
+
+main = xmonad myConfig
+
+myManageHook = composeAll
+  [ className =? "yakuake" --> doFloat  
+  , className =? "Yakuake" --> doFloat  
+  , className =? "Kmix" --> doFloat  
+  , className =? "kmix" --> doFloat  
+  , className =? "plasma" --> doFloat  
+  , className =? "Plasma" --> doFloat  
+  , className =? "plasma-desktop" --> doFloat  
+  , className =? "Plasma-desktop" --> doFloat  
+  , className =? "krunner" --> doFloat  
+  , className =? "ksplashsimple" --> doFloat  
+  , className =? "ksplashqml" --> doFloat  
+  , className =? "ksplashx" --> doFloat  
+  , className =? "plasmashell" --> doFloat  
+  ]  
+
+myConfig = kdeConfig
+        { modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , manageHook = manageDocks <+> myManageHook <+> manageHook kdeConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        } 
