f07046729c9e107f7e1cc862afca07d639be6a77
Boris Babic <boris.ivan.babic@gmail.com>
2018-07-06 09:32:41 +0200

    NIXOS: Add x1_6 throttling fix

--- nixos/config/x1_6.nix
+++ nixos/config/x1_6.nix
@@ -71,6 +71,39 @@
     bluez = pkgs.bluez5;
   };
 
+  # copied from https://github.com/NixOS/nixos-hardware/pull/60/files#diff-df1e8b81c371ec5227fc57076aa132ca
+  systemd.services.cpu-throttling = {
+    enable = true;
+    description = "Sets the offset to 3 °C, so the new trip point is 97 °C";
+    documentation = [
+      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+    ];
+    path = [ pkgs.msr-tools ];
+    script = "wrmsr -a 0x1a2 0x3000000";
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    wantedBy = [
+      "timers.target"
+    ];
+  };
+
+  # copied from https://github.com/NixOS/nixos-hardware/pull/60/files#diff-df1e8b81c371ec5227fc57076aa132ca
+  systemd.timers.cpu-throttling = {
+    enable = true;
+    description = "Set cpu heating limit to 97 °C";
+    documentation = [
+      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+    ];
+    timerConfig = {
+      OnActiveSec = 60;
+      OnUnitActiveSec = 60;
+      Unit = "cpu-throttling.service";
+    };
+    wantedBy = [
+      "timers.target"
+    ];
+  };
 
   powerManagement = {
     enable = true;
