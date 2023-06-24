8d7ae5cac831585cf869f92422bcace631555fea
Boris Babic <boris.ivan.babic@gmail.com>
2023-06-24 08:17:33 +0200

    AWM4: Change tiling

--- awesome4/.config/awesome/rc.lua
+++ awesome4/.config/awesome/rc.lua
@@ -95,9 +95,9 @@ awful.util.terminal = terminal
 awful.util.tagnames = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 awful.layout.layouts = {
     -- awful.layout.suit.floating,
-    awful.layout.suit.tile
     -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile,
     -- awful.layout.suit.tile.top,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
@@ -284,6 +284,8 @@ awful.screen.connect_for_each_screen(
     end
 )
 -- }}}
+--
+--awful.screen.set_auto_dpi_enabled( true )
 
 -- {{{ Mouse bindings
 root.buttons(
