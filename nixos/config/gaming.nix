177df0653510c143a71a72e17deafd6ad46157bf
Boris <boris.ivan.babic@gmail.com>
2026-01-27 21:29:46 +0100

    NIXOS: Fix steam gui scaling

--- nixos/config/gaming.nix
+++ nixos/config/gaming.nix
@@ -9,4 +9,10 @@
     bottles
     lutris
   ];
+  environment.sessionVariables = {
+    # Force Steam to stop trying to be smart
+    "STEAM_FORCE_DESKTOPUI_SCALING" = "1.0";
+    # Tell XWayland apps (like Steam) not to scale themselves
+    "GDK_SCALE" = "1";
+  };
 }
