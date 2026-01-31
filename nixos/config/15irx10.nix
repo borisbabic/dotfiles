80a8fc2034ea28b6adc09ada8ae576ca8250bd50
Boris <boris.ivan.babic@gmail.com>
2026-01-31 13:17:43 +0100

    NIXOS: Try to fix legion 5 unsuspend issues

--- nixos/config/15irx10.nix
+++ nixos/config/15irx10.nix
@@ -38,4 +38,15 @@
       sync.enable = lib.mkForce false;
     };
   };
+  # try to fix wifi issue. Suggested by gemini
+  hardware.enableRedistributableFirmware = true;
+
+  ###### <fix hibernate, suggested by gemini>
+  hardware.nvidia.powerManagement.enable = true;
+  # This option can sometimes help with "black screen" resume issues
+  # by preserving video memory in /tmp
+  hardware.nvidia.powerManagement.finegrained = false;
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  ###### </fix hibernate, suggested by gemini>
+
 }
