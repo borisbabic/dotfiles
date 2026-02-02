6caa89520259802dd4b4932abfc40ef2e5d24b4e
Boris <boris.ivan.babic@gmail.com>
2026-02-02 05:21:36 +0100

    WAYBAR: Add spotify module

--- nixos/config/main.nix
+++ nixos/config/main.nix
@@ -181,6 +181,7 @@
     nix-index
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     pavucontrol
+    playerctl
   ];
 
   fonts.packages = with pkgs; [
