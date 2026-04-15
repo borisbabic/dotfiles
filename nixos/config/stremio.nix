c1a6d8876ed5ce22478f8dac24bed3918e68f712
Boris Babic <boris.ivan.babic@gmail.com>
2026-04-16 01:00:56 +0200

    NIXOS: Fix stremio service

--- nixos/config/stremio.nix
+++ nixos/config/stremio.nix
@@ -8,6 +8,7 @@
       ExecStart = "${pkgs.stremio-service}/bin/stremio-service";
       Restart = "always";
     };
+    path = [pkgs.procps];
   };
   environment.systemPackages = with pkgs; [
     stremio-service
