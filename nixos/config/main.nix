f2be7e2e907ce556b043b1d39ebb634413e048f1
Boris Babic <boris.ivan.babic@gmail.com>
2026-07-29 10:01:16 +0200

    NIXOS: Enable nvenc in OBS

--- nixos/config/main.nix
+++ nixos/config/main.nix
@@ -252,6 +252,9 @@
   programs.obs-studio.enableVirtualCamera = true;
   programs.obs-studio = {
     enable = true;
+    package = pkgs.obs-studio.override {
+      cudaSupport = true;
+    };
     plugins = with pkgs.obs-studio-plugins; [
       obs-backgroundremoval
     ];
