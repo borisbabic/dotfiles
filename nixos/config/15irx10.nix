207348dfe2b42e67dfbc0c725130198cbe8c1629
Boris Babic <boris.ivan.babic@gmail.com>
2026-08-02 03:51:22 +0200

    NIXOS: Remove garnix

--- nixos/config/15irx10.nix
+++ nixos/config/15irx10.nix
@@ -8,9 +8,6 @@
 # sudo legion_cli --donotexpecthwmon batteryconservation-disable
 
 {
-  # for cachyos kernels
-  nix.settings.substituters = [ "https://cache.garnix.io" "https://attic.xuyh0120.win/lantian" ];
-  nix.settings.trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
   # Use latest kernel.
   #boot.kernelPackages = pkgs.linuxPackages_latest;
 
