688d1c619b32849269413b3d488b569e574be81a
Boris Babic <boris.ivan.babic@gmail.com>
2026-08-12 11:24:40 +0200

    HYPR: Update config

--- hyprland-lua/.config/hypr/keybinds.lua
+++ hyprland-lua/.config/hypr/keybinds.lua
@@ -1,4 +1,3 @@
-
 ---------------------
 ---- KEYBINDINGS ----
 ---------------------
@@ -6,153 +5,186 @@
 local variables = require("variables")
 local shieldIP = variables.shieldIP
 local mainMod = variables.mainMod
-local terminal    = variables.terminal
+local terminal = variables.terminal
 local fileManager = variables.fileManager
-local menu        = variables.menu
+local menu = variables.menu
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + SHIFT + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
-hl.bind(mainMod .. " + SHIFT + Q", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(
+	mainMod .. " + SHIFT + Q",
+	hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'")
+)
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + SHIFT +  F", hl.dsp.window.float({ action = "toggle", internal = true }))
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + SHIFT + CTRL + X", hl.dsp.exec_cmd("hyprctl kill"))
-hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen())
-hl.bind(mainMod .. " + CTRL + F", hl.dsp.window.fullscreen_state({internal = 0, client = 2}))
-hl.bind(mainMod .. " + CTRL + SHIFT + L", hl.dsp.window.resize({x = 2560/1.6, y =1600/1.6}))
+hl.bind(mainMod .. " + CTRL + F", hl.dsp.window.fullscreen_state({ internal = 0, client = 2 }))
+hl.bind(mainMod .. " + CTRL + SHIFT + L", hl.dsp.window.resize({ x = 2560 / 1.6, y = 1600 / 1.6 }))
 hl.bind(mainMod .. " + CTRL + P", hl.dsp.window.pin())
-hl.bind(mainMod .. " + SHIFT + P", function ()
-  hl.dispatch(hl.dsp.window.float({action = "toggle"}))
-  hl.dispatch(hl.dsp.window.pin())
+hl.bind(mainMod .. " + SHIFT + P", function()
+	hl.dispatch(hl.dsp.window.float({ action = "toggle" }))
+	hl.dispatch(hl.dsp.window.pin())
 end)
 
-
 -- Directional keybinds
-for _, dir in ipairs({"left", "right", "up", "down"}) do
-  hl.bind(mainMod .. " + " .. dir, hl.dsp.focus({direction = dir}))
-  hl.bind(mainMod .. " + SHIFT + " .. dir, hl.dsp.window.move({direction = dir}))
+for _, dir in ipairs({ "left", "right", "up", "down" }) do
+	hl.bind(mainMod .. " + " .. dir, hl.dsp.focus({ direction = dir }))
+	hl.bind(mainMod .. " + SHIFT + " .. dir, hl.dsp.window.move({ direction = dir }))
 end
 
-
-
 local function move_window_to(workspace)
-  return function()
-    local ws = hl.get_active_special_workspace() or hl.get_active_workspace()
-    local follow = ws ~= nil and ws.windows < 2
-    hl.dispatch(HS.dsp.window.move({workspace = workspace, follow = follow}))
-  end
+	return function()
+		local ws = hl.get_active_special_workspace() or hl.get_active_workspace()
+		local follow = ws ~= nil and ws.windows < 2
+		hl.dispatch(HS.dsp.window.move({ workspace = workspace, follow = follow }))
+	end
 end
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
 --
 for i = 1, 9 do
-    local key = i % 10 -- 10 maps to key 0
-    -- hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
-    -- hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
-    hl.bind(mainMod .. " + " .. key,             HS.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + SHIFT + " .. key,     move_window_to(i))
+	local key = i % 10 -- 10 maps to key 0
+	-- hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
+	-- hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+	hl.bind(mainMod .. " + " .. key, HS.dsp.focus({ workspace = i }))
+	hl.bind(mainMod .. " + SHIFT + " .. key, move_window_to(i))
 end
 
-
 hl.bind(mainMod .. "+ G", HS.dsp.grab_rogue_windows())
-hl.bind(mainMod ..  " + SHIFT + D", HS.dsp.workspace.swap_monitors({ monitor1 = "current", monitor2 = "+1" }))
+hl.bind(mainMod .. " + SHIFT + D", HS.dsp.workspace.swap_monitors({ monitor1 = "current", monitor2 = "+1" }))
 hl.bind(mainMod .. " + CTRL + H", hl.dsp.exec_cmd("env LUTRIS_SKIP_INIT=1 lutris lutris:rungameid/17"))
-hl.bind(mainMod .. " + CTRL + D", hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_dev", {workspace = "22"}))
-hl.bind(mainMod .. " + CTRL + R", hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_remote", {workspace = "13"}))
-hl.bind(mainMod .. " + CTRL + SHIFT + D", hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_dev"))
-hl.bind(mainMod .. " + CTRL + SHIFT + R", hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_remote"))
+hl.bind(
+	mainMod .. " + CTRL + D",
+	hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_dev", { workspace = "22" })
+)
+hl.bind(
+	mainMod .. " + CTRL + R",
+	hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_remote", { workspace = "13" })
+)
+hl.bind(
+	mainMod .. " + CTRL + SHIFT + D",
+	hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_dev")
+)
+hl.bind(
+	mainMod .. " + CTRL + SHIFT + R",
+	hl.dsp.exec_cmd("kitty --session ~/dotfiles/kitty/.config/kitty/sessions/hsguru_remote")
+)
 
-
-hl.bind("ALT + ALT_R + C", hl.dsp.exec_cmd("chatterino"), {release = true})
+hl.bind("ALT + ALT_R + C", hl.dsp.exec_cmd("chatterino"), { release = true })
 -- Example special workspace (scratchpad)
-hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
+hl.bind(mainMod .. " + S", hl.dsp.workspace.toggle_special("magic"))
 hl.bind(mainMod .. " + SHIFT + S", move_window_to("special:magic"))
 
-hl.bind(mainMod .. " + G",         hl.dsp.workspace.toggle_special("game"))
+hl.bind(mainMod .. " + G", hl.dsp.workspace.toggle_special("game"))
 hl.bind(mainMod .. " + SHIFT + G", move_window_to("special:game"))
 
 -- Scroll through existing workspaces with mainMod + scroll
 hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
-hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
+hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
-hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 -- hl.bind(mainMod .. " + SHIFT + mouse:273", hl.dsp.window.resize(), { mouse = true, prop = {keep_aspect_ratio = true }})
 hl.bind(mainMod .. " + SHIFT + mouse:273", function()
-  local active = hl.get_active_window()
-  if active ~= nil then
-    hl.dispatch(hl.dsp.window.set_prop({prop = "keep_aspect_ratio", value = "1"}))
-    hl.dispatch(hl.dsp.window.resize())
-  end
-end, { mouse = true})
-hl.bind(mainMod .. " + SHIFT + mouse:273", hl.dsp.window.set_prop({prop = "keep_aspect_ratio", value = "0"}), { mouse = true, release = true})
+	local active = hl.get_active_window()
+	if active ~= nil then
+		hl.dispatch(hl.dsp.window.set_prop({ prop = "keep_aspect_ratio", value = "1" }))
+		hl.dispatch(hl.dsp.window.resize())
+	end
+end, { mouse = true })
+hl.bind(
+	mainMod .. " + SHIFT + mouse:273",
+	hl.dsp.window.set_prop({ prop = "keep_aspect_ratio", value = "0" }),
+	{ mouse = true, release = true }
+)
 -- Laptop multimedia keys for volume and LCD brightness
 -- hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
 -- hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
 -- hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind(
+	"XF86AudioMicMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+	{ locked = true, repeating = true }
+)
 -- hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
 -- hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
 
 -- Requires playerctl
-hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 
+-- Send shortcuts to specific programs
+ANTI_GRAVITY_RUN = function()
+	hl.dispatch(hl.dsp.send_shortcut({
+		mods = "ALT",
+		key = "return",
+		window = "class:antigravity",
+	}))
+end
+hl.bind(mainMod .. " + SHIFT + RETURN", ANTI_GRAVITY_RUN)
 
 -----SHIELD
 local shieldBase = "adb connect " .. shieldIP .. " && adb shell "
 for keys, keyevent in pairs({
-  ["XF86AudioPause"] = "85",
-  ["XF86AudioPlay"] = "85",
-  ["XF86AudioNext"] = "87",
-  ["XF86AudioRaiseVolume"] = "24",
-  ["XF86AudioLowerVolume"] = "25"
+	["XF86AudioPause"] = "85",
+	["XF86AudioPlay"] = "85",
+	["XF86AudioNext"] = "87",
+	["XF86AudioRaiseVolume"] = "24",
+	["XF86AudioLowerVolume"] = "25",
 }) do
-  hl.bind(mainMod ..  " + "  .. keys, hl.dsp.exec_cmd(shieldBase .. "input keyevent " .. keyevent), {locked = true})
+	hl.bind(mainMod .. " + " .. keys, hl.dsp.exec_cmd(shieldBase .. "input keyevent " .. keyevent), { locked = true })
 end
 
 for key, keyevent in pairs({
-  ["w"] = "KEYCODE_WAKEUP",
-  ["up"] = "19",
-  ["down"] = "20",
-  ["left"] = "21",
-  ["right"] = "22",
-  -- dpad center
-  ["return"] = "23",
-  ["backspace"] = "4",
-  ["kp_home"] = "3",
-  ["home"] = "3",
-  -- Passthrough
-  ["CTRL + backspace"] = "67",
-  ["CTRL + return"] = "66",
-  ["CTRL + space"] = "62",
-  -- app switcer
-  ["kp_end"] = "187",
-  ["end"] = "187",
-  ["tab"] = "61",
-  ["SHIFT + tab"]  = "143"
+	["w"] = "KEYCODE_WAKEUP",
+	["up"] = "19",
+	["down"] = "20",
+	["left"] = "21",
+	["right"] = "22",
+	-- dpad center
+	["return"] = "23",
+	["backspace"] = "4",
+	["kp_home"] = "3",
+	["home"] = "3",
+	-- Passthrough
+	["CTRL + backspace"] = "67",
+	["CTRL + return"] = "66",
+	["CTRL + space"] = "62",
+	-- app switcer
+	["kp_end"] = "187",
+	["end"] = "187",
+	["tab"] = "61",
+	["SHIFT + tab"] = "143",
 }) do
-  hl.bind(mainMod .. " + ALT + "  .. key, hl.dsp.exec_cmd(shieldBase .. "input keyevent " .. keyevent))
+	hl.bind(mainMod .. " + ALT + " .. key, hl.dsp.exec_cmd(shieldBase .. "input keyevent " .. keyevent))
 end
 
 for i = string.byte("a"), string.byte("z") do
-    local letter = string.char(i)
-    hl.bind(mainMod .. " + CTRL + ALT + " .. letter, hl.dsp.exec_cmd(shieldBase .. "input keyevent " .. (i - 68)))
+	local letter = string.char(i)
+	hl.bind(mainMod .. " + CTRL + ALT + " .. letter, hl.dsp.exec_cmd(shieldBase .. "input keyevent " .. (i - 68)))
 end
 
 -- opens url from clipboard on shield's browser
-hl.bind(mainMod .. " + ALT  + b", hl.dsp.exec_cmd(shieldBase .. "am start -a android.intent.action.VIEW -d \"$(wl-paste | tr -d '[:space:]')\""))
+hl.bind(
+	mainMod .. " + ALT  + b",
+	hl.dsp.exec_cmd(shieldBase .. "am start -a android.intent.action.VIEW -d \"$(wl-paste | tr -d '[:space:]')\"")
+)
 for keys, keyevent in pairs({
-  ["s"] = "com.stremio.one",
-  ["y"] = "io.gh.reisxd.tizentube.cobalt",
-  ["t"] = "twitch.android.app"
+	["s"] = "com.stremio.one",
+	["y"] = "io.gh.reisxd.tizentube.cobalt",
+	["t"] = "twitch.android.app",
 }) do
-  hl.bind(mainMod ..  " + ALT + "  .. keys, hl.dsp.exec_cmd(shieldBase .. "monkey -p " .. keyevent .. " -c android.intent.category.LAUNCHER 1"))
+	hl.bind(
+		mainMod .. " + ALT + " .. keys,
+		hl.dsp.exec_cmd(shieldBase .. "monkey -p " .. keyevent .. " -c android.intent.category.LAUNCHER 1")
+	)
 end
--- hyprland-lua/.config/hypr/util.lua
+++ hyprland-lua/.config/hypr/util.lua
@@ -1,44 +1,96 @@
-
 local function serializeTable(val, name, skipnewlines, depth)
-    skipnewlines = skipnewlines or false
-    depth = depth or 0
+	skipnewlines = skipnewlines or false
+	depth = depth or 0
 
-    local tmp = string.rep(" ", depth)
+	local tmp = string.rep(" ", depth)
+	hl.dispatch(hl.dsp.dpms())
 
-    if name then tmp = tmp .. name .. " = " end
+	if name then
+		tmp = tmp .. name .. " = "
+	end
 
-    if type(val) == "table" then
-        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+	if type(val) == "table" then
+		tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
 
-        for k, v in pairs(val) do
-            tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
-        end
+		for k, v in pairs(val) do
+			tmp = tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+		end
 
-        tmp = tmp .. string.rep(" ", depth) .. "}"
-    elseif type(val) == "number" then
-        tmp = tmp .. tostring(val)
-    elseif type(val) == "string" then
-        tmp = tmp .. string.format("%q", val)
-    elseif type(val) == "boolean" then
-        tmp = tmp .. (val and "true" or "false")
-    else
-        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
-    end
+		tmp = tmp .. string.rep(" ", depth) .. "}"
+	elseif type(val) == "number" then
+		tmp = tmp .. tostring(val)
+	elseif type(val) == "string" then
+		tmp = tmp .. string.format("%q", val)
+	elseif type(val) == "boolean" then
+		tmp = tmp .. (val and "true" or "false")
+	else
+		tmp = tmp .. '"[inserializeable datatype:' .. type(val) .. ']"'
+	end
 
-    return tmp
+	return tmp
 end
 local notif = function(text_or_table, duration)
-
-  local text = text_or_table
-  if type(text_or_table)  == "table" then
-    text = serializeTable(text_or_table)
-  end
-  text = text or "Empty Notification"
-  hl.notification.create({duration = duration or 5000, text = text})
+	local text = text_or_table
+	if type(text_or_table) == "table" then
+		text = serializeTable(text_or_table)
+	end
+	text = text or "Empty Notification"
+	hl.notification.create({ duration = duration or 5000, text = text, timeout = duration or 5000 })
 end
-return {
-  notif = notif,
-  notify = notif,
-  serializeTable = serializeTable
-}
 
+local function disableMonitors(monitor_names)
+	for _, v in ipairs(monitor_names) do
+		print(v)
+		hl.monitor({ output = v, disabled = true })
+	end
+end
+local function enableMonitors(monitor_names)
+	for _, v in ipairs(monitor_names) do
+		print(v)
+		hl.monitor({ output = v, disabled = false })
+	end
+end
+local function monitorDisabler(monitor_names)
+	return function()
+		disableMonitors(monitor_names)
+	end
+end
+local function monitorEnabler(monitor_names)
+	return function()
+		enableMonitors(monitor_names)
+	end
+end
+local function prioritizeMonitor(name)
+	local monitors = hl.get_monitors()
+	local preceding = {}
+	local monitor_enabled = false
+	for _, v in ipairs(monitors) do
+		if v["name"] == name then
+			monitor_enabled = true
+		elseif not monitor_enabled then
+			table.insert(preceding, v["name"])
+		end
+	end
+	print(preceding)
+	print(monitor_enabled)
+	if monitor_enabled then
+		-- if we don't dispatch nothing happens
+		hl.dispatch(monitorDisabler(preceding))
+		hl.dispatch(monitorEnabler(preceding))
+	end
+end
+
+local function test(name)
+	hl.monitor({ output = name or "DP-2", disabled = true })
+	-- hl.monitor({ output = name or "DP-2", disabled = false })
+end
+
+return {
+	notif = notif,
+	notify = notif,
+	scratch = test,
+	prioritizeMonitor = prioritizeMonitor,
+	enableMonitors = enableMonitors,
+	disableMonitors = disableMonitors,
+	serializeTable = serializeTable,
+}
