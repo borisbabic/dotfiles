4f551d25878302f2d634253616205336a3488adb
Boris Babic <boris.ivan.babic@gmail.com>
2026-08-31 00:26:31 +0200

    HYPR: Remove duplicate dms

--- hyprland-lua/.config/hypr/hyprland.lua
+++ hyprland-lua/.config/hypr/hyprland.lua
@@ -41,6 +41,7 @@ require("autostart")
 -------------------------------
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+--
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
